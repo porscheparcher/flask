3f81a8739b8edaf5204a7a4b87c3b9418dc6d558
Porsche Parcher <130427863+porscheparcher@users.noreply.github.com>
2023-08-14 13:35:25 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask
+# app.py
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/welcome')
-def welcome():
-    return 'welcome'
+# Import the helper functions from operations.py
+from operations import add, sub, mult, div
 
-@app.route('/welcome/home')
-def welcome_home():
-    return 'welcome home'
+@app.route('/add')
+def addition():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a, b)
+    return str(result)
 
-@app.route('/welcome/back')
-def welcome_back():
-    return "welcome back"
+@app.route('/sub')
+def subtraction():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a, b)
+    return str(result)
+
+@app.route('/mult')
+def multiplication():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a, b)
+    return str(result)
+
+@app.route('/div')
+def division():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a, b)
+    return str(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
